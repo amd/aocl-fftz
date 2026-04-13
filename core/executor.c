0717ba7314993dd27ca6dbbb23811a6431df7cf6
agodbole_amdeng <Ashwin.Godbole@amd.com>
2026-04-13 14:26:02 +0530

    fix: openmp overhead when running single threaded workloads

--- core/executor.c
+++ core/executor.c
@@ -17,20 +17,28 @@
 static INT32 execute_dft(aoclfftz_executor_t *executor_obj)
 {
     aoclfftz_solution_t *sol = executor_obj->solution;
+
 #ifdef MULTI_THREADING
-    // Retrive max nested levels from master application
-    UINT32 cur_max_levels = omp_get_max_active_levels();
+    INT32 threads = sol->decomp_scheme->thread_info->pthr_fft->num_threads;
 
-    // Set maximum nested levels to 3 as it is needed by real solutions
-    omp_set_max_active_levels(3);
-    INT32 ret = sol->solver->execute_solver(sol);
+    if (threads > 1)
+    {
+        // Retrieve max nested levels from master application
+        UINT32 cur_max_levels = omp_get_max_active_levels();
 
-    // Restore max nested levels to original state
-    omp_set_max_active_levels(cur_max_levels);
-    return ret;
-#else
-    return sol->solver->execute_solver(sol);
+        // Set maximum nested levels to 3 as it is needed by real solutions
+        omp_set_max_active_levels(3);
+        INT32 ret = sol->solver->execute_solver(sol);
+
+        // Restore max nested levels to original state
+        omp_set_max_active_levels(cur_max_levels);
+        return ret;
+    }
+    else
 #endif
+    {
+        return sol->solver->execute_solver(sol);
+    }
 }
 
 execute_ register_execute_dft(VOID)
